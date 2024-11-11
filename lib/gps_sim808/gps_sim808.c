7e64f4afc0180530957ae9722ebcfe8a433d2b7d
Diego <diegorydez@gmail.com>
2024-11-11 19:16:34 +0100

    Añadida funcion de monitorización de batería. Modificada velocidad de comunicación uart.

--- lib/gps_sim808/gps_sim808.c
+++ lib/gps_sim808/gps_sim808.c
@@ -1,11 +1,21 @@
 #include "gps_sim808.h"
 #include "driver/uart.h"
 #include <string.h>
+#include <stdio.h>
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #define UART_NUM UART_NUM_1  // UART 1 para comunicación con SIM808
 #define TXD_PIN (17)
 #define RXD_PIN (16)
-#define BAUD_RATE 115200
+#define BAUD_RATE 9600
+#define BUF_SIZE           1024
+#define VBAT_COMMAND       "AT+CBC\r\n"
+
+void print_command(const char *command) {
+    printf("Se envió al SIM808: %s\n", command);
+}
 
 int sim808_init() {
     // Configura UART
@@ -36,6 +46,7 @@ int sim808_init() {
 
 void sim808_send_command(const char *command) {
     uart_write_bytes(UART_NUM, command, strlen(command));
+    print_command(command);
 }
 
 void sim808_read_response(char *buffer, size_t buffer_size) {
@@ -44,5 +55,35 @@ void sim808_read_response(char *buffer, size_t buffer_size) {
         buffer[length] = '\0';  // Termina el string
     } else {
         buffer[0] = '\0';  // No hay datos
+        printf("No se recibió nada en el buffer de recepción.\n");
+    }
+}
+
+// Función para enviar el comando AT+CBC y recibir el voltaje de la batería
+void monitorizar_vbat() {
+    // Enviar el comando AT+CBC
+    uart_write_bytes(UART_NUM, (const char *)VBAT_COMMAND, strlen(VBAT_COMMAND));
+    printf("Enviando comando: %s\n", VBAT_COMMAND);
+
+    // Buffer para leer la respuesta
+    char data[BUF_SIZE];
+    int length = uart_read_bytes(UART_NUM, (uint8_t *)data, BUF_SIZE - 1, 100 / portTICK_PERIOD_MS);
+
+    // Asegurarse de que haya datos leídos
+    if (length > 0) {
+        data[length] = '\0';  // Añadir el terminador nulo para convertir a cadena de caracteres
+        printf("Respuesta del módulo: %s\n", data);
+
+        // Buscar el valor del voltaje en la respuesta
+        char *start = strstr(data, "+CBC: ");
+        if (start) {
+            start += 7;  // Desplazar el puntero después de "+CBC: "
+            int battery_voltage = atoi(start);  // Convertir la parte de la respuesta que contiene el voltaje a entero
+            printf("Voltaje de la batería: %d mV\n", battery_voltage);
+        } else {
+            printf("No se pudo obtener el voltaje de la batería.\n");
+        }
+    } else {
+        printf("No se recibió respuesta o hubo un error al leer.\n");
     }
 }
--- src/main.c
+++ src/main.c
@@ -18,7 +18,8 @@ void app_main(void) {
             printf("Latitud: %f\n", gps_data.latitude);
             printf("Longitud: %f\n", gps_data.longitude);
         }
-
+        
+        monitorizar_vbat();
        // Retraso de 1 segundo
         esp_rom_delay_us(1000000);   // 1000 ms = 1000000 us
     }
